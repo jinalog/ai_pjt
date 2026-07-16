9a5cc1b0804559698e41c5e29c4737037cee0a40
jina <47252423+jinalog@users.noreply.github.com>
2026-07-16 10:55:07 +0900

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -30,7 +30,7 @@ function changePage(page) {
 
 const selectedImage = ref(null)
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ai-pjt-1.onrender.com'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ai-pjt.onrender.com'
 const page = ref('onboarding')
 const nickname = ref('')
 const password = ref('')
