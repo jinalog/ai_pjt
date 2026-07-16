412b2661fbcc6bea307f5c33ccdc71ce44945e14
jina <47252423+jinalog@users.noreply.github.com>
2026-07-16 11:04:31 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -292,7 +292,7 @@ app = FastAPI(title="AI Team8 Travel API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://ai-pjt-1.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
